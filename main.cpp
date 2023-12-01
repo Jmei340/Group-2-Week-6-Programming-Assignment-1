a64f24dc5e2d6709a7e1ade8326c31eba15a5eca
Jmei340 <148639086+Jmei340@users.noreply.github.com>
2023-12-01 14:37:46 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+// program name goes here
+// your name goes here
+// any example usage or helpful command line arguments go here
+
+#include <iostream>
+// Add any other header files here
+
+///////////////////////////////////////
+int main()
+{
+    // Work here
+
+    // Return 0 to indicate success.
+    return 0;
+}
+
+// Place any unused code here so that the instructor can follow your problem solving thoughts.
