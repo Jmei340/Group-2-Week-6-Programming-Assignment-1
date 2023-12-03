3e4a48557e650cc63418a1c49b588f8959ad15c7
Jmei340 <meij340@macomb.edu>
2023-12-03 16:00:27 -0500

    Made Finishing Changes

--- main.cpp
+++ main.cpp
@@ -14,26 +14,23 @@ using namespace std;
 void ReadStoreData(string Storing[50], string Names[10], int Scores[10][5]) {
     // Declaring Variables
     string element;
-    int rows;
     int i = 0;
     int index = 0;
     int index_2 = 0;
     int index_3 = 0;
     int index_4 = 0;
     int j = 0;
-    int columns;
     int Skip = 0;
 
     // Getting File
     fstream infile;
     infile.open("BowlingScores.txt");
 
-    // Storing Values to Seperate Int and Strings
+    // Check if file is open
     if (infile.is_open()) {
         for (i = 0; i < 50; i++) {
             infile >> Storing[i];
         }
-    }
 
     // Storing Names
     for (j = 0; j < 10; j++) {
@@ -95,6 +92,12 @@ void ReadStoreData(string Storing[50], string Names[10], int Scores[10][5]) {
             }
         }
     }
+    }
+
+    // If File Failed To Open
+    else {
+        cout << "File Failed To Open" << endl;
+    }
 }
 
 void CalculatingAverage(int Scores[10][5], int AverageScores[10]) {
@@ -102,54 +105,190 @@ void CalculatingAverage(int Scores[10][5], int AverageScores[10]) {
     int rows;
     int columns;
     const int AverageConversion = 4;
+    int index = 0;
+    int index0 = 0;
+    int index1 = 0;
+    int index2 = 0;
+    int Average = 0;
+    int total = 0;
 
-
-
-
+    // Calculating Average Scores
+    for (index0 = 0; index0 < 10; index0++) {
+        if (index == 0) {
+            for (index1 = 0; index1 < 5; index1++) {
+                total = total + Scores[0][index1];
+                Average = total / AverageConversion;
+            }
+            AverageScores[0] = Average;
+            index++;
+            index1 = 0;
+            total = 0;
+        }
+        else if (index == 1) {
+            for (index1 = 0; index1 < 5; index1++) {
+                total = total + Scores[1][index1];
+                Average = total / AverageConversion;
+            }
+            AverageScores[1] = Average;
+            index++;
+            index1 = 0;
+            total = 0;
+        }
+        else if (index == 2) {
+            for (index1 = 0; index1 < 5; index1++) {
+                total = total + Scores[2][index1];
+                Average = total / AverageConversion;
+            }
+            AverageScores[2] = Average;
+            index++;
+            index1 = 0;
+            total = 0;
+        }
+        else if (index == 3) {
+            for (index1 = 0; index1 < 5; index1++) {
+                total = total + Scores[3][index1];
+                Average = total / AverageConversion;
+            }
+            AverageScores[3] = Average;
+            index++;
+            index1 = 0;
+            total = 0;
+        }
+        else if (index == 4) {
+            for (index1 = 0; index1 < 5; index1++) {
+                total = total + Scores[4][index1];
+                Average = total / AverageConversion;
+            }
+            AverageScores[4] = Average;
+            index++;
+            index1 = 0;
+            total = 0;
+        }
+        else if (index == 5) {
+            for (index1 = 0; index1 < 5; index1++) {
+                total = total + Scores[5][index1];
+                Average = total / AverageConversion;
+            }
+            AverageScores[5] = Average;
+            index++;
+            index1 = 0;
+            total = 0;
+        }
+        else if (index == 6) {
+            for (index1 = 0; index1 < 5; index1++) {
+                total = total + Scores[6][index1];
+                Average = total / AverageConversion;
+            }
+            AverageScores[6] = Average;
+            index++;
+            index1 = 0;
+            total = 0;
+        }
+        else if (index == 7) {
+            for (index1 = 0; index1 < 5; index1++) {
+                total = total + Scores[7][index1];
+                Average = total / AverageConversion;
+            }
+            AverageScores[7] = Average;
+            index++;
+            index1 = 0;
+            total = 0;
+        }
+        else if (index == 8) {
+            for (index1 = 0; index1 < 5; index1++) {
+                total = total + Scores[8][index1];
+                Average = total / AverageConversion;
+            }
+            AverageScores[8] = Average;
+            index++;
+            index1 = 0;
+            total = 0;
+        }
+        else if (index == 9) {
+            for (index1 = 0; index1 < 5; index1++) {
+                total = total + Scores[9][index1];
+                Average = total / AverageConversion;
+            }
+            AverageScores[9] = Average;
+            index++;
+            index1 = 0;
+            total = 0;
+        }
+    }
 }
 
-void OutputResults(string Names[10], int Scores[10][4], int AverageScores[10]) {
+void OutputResults(string Names[10], int Scores[10][5], int AverageScores[10]) {
     //Outputs
 
     // Bowler #1
-    cout << Names[0] << "." << setw(18) << setfill('.') << Scores[0][0] << setw(7) << setfill('.') << Scores[0][1] << setw(7) << setfill('.')
-        << Scores[0][2] << setw(8) << setfill('.') << Scores[0][3] << setw(5) << setfill('.') << AverageScores[0] << endl;
+    cout << Names[0] << "." << setw(15) << setfill('.') << Scores[0][1] << setw(10) << setfill('.') << Scores[0][2] << setw(10) << setfill('.')
+        << Scores[0][3] << setw(11) << setfill('.') << Scores[0][4] << setw(8) << setfill('.') << AverageScores[0] << endl;
 
     //Bowler #2
-    cout << Names[1] << "." << setw(17) << setfill('.') << Scores[1][0] << setw(8) << setfill('.') << Scores[1][1] << setw(9) << setfill('.')
-        << Scores[1][2] << setw(8) << setfill('.') << Scores[1][3] << setw(6) << setfill('.') << AverageScores[1] << endl;
+    cout << Names[1] << "." << setw(13) << setfill('.') << Scores[1][1] << setw(10) << setfill('.') << Scores[1][2] << setw(10) << setfill('.')
+        << Scores[1][3] << setw(11) << setfill('.') << Scores[1][4] << setw(8) << setfill('.') << AverageScores[1] << endl;
 
     //Bowler #3
-    cout << Names[2] << "." << setw(20) << setfill('.') << Scores[2][0] << setw(7) << setfill('.') << Scores[2][1] << setw(7) << setfill('.')
-        << Scores[2][2] << setw(8) << setfill('.') << Scores[2][3] << setw(5) << setfill('.') << AverageScores[2] << endl;
+    cout << Names[2] << "." << setw(12) << setfill('.') << Scores[2][1] << setw(10) << setfill('.') << Scores[2][2] << setw(10) << setfill('.')
+        << Scores[2][3] << setw(11) << setfill('.') << Scores[2][4] << setw(8) << setfill('.') << AverageScores[2] << endl;
 
     //Bowler #4
-    cout << Names[3] << "." << setw(20) << setfill('.') << Scores[3][0] << setw(7) << setfill('.') << Scores[3][1] << setw(7) << setfill('.')
-        << Scores[3][2] << setw(8) << setfill('.') << Scores[3][3] << setw(5) << setfill('.') << AverageScores[3] << endl;
+    cout << Names[3] << "." << setw(13) << setfill('.') << Scores[3][1] << setw(10) << setfill('.') << Scores[3][2] << setw(10) << setfill('.')
+        << Scores[3][3] << setw(11) << setfill('.') << Scores[3][4] << setw(8) << setfill('.') << AverageScores[3] << endl;
 
     //Bowler #5
-    cout << Names[4] << "." << setw(20) << setfill('.') << Scores[4][0] << setw(7) << setfill('.') << Scores[4][1] << setw(7) << setfill('.')
-        << Scores[4][2] << setw(8) << setfill('.') << Scores[4][3] << setw(5) << setfill('.') << AverageScores[4] << endl;
+    cout << Names[4] << "." << setw(13) << setfill('.') << Scores[4][1] << setw(10) << setfill('.') << Scores[4][2] << setw(10) << setfill('.')
+        << Scores[4][3] << setw(11) << setfill('.') << Scores[4][4] << setw(8) << setfill('.') << AverageScores[4] << endl;
 
     //Bowler #6
-    cout << Names[5] << "." << setw(20) << setfill('.') << Scores[5][0] << setw(7) << setfill('.') << Scores[5][1] << setw(7) << setfill('.')
-        << Scores[5][2] << setw(8) << setfill('.') << Scores[5][3] << setw(5) << setfill('.') << AverageScores[5] << endl;
+    cout << Names[5] << "." << setw(12) << setfill('.') << Scores[5][1] << setw(10) << setfill('.') << Scores[5][2] << setw(10) << setfill('.')
+        << Scores[5][3] << setw(11) << setfill('.') << Scores[5][4] << setw(8) << setfill('.') << AverageScores[5] << endl;
 
     //Bowler #7
-    cout << Names[6] << "." << setw(20) << setfill('.') << Scores[6][0] << setw(7) << setfill('.') << Scores[6][1] << setw(7) << setfill('.')
-        << Scores[6][2] << setw(8) << setfill('.') << Scores[6][3] << setw(5) << setfill('.') << AverageScores[6] << endl;
+    cout << Names[6] << "." << setw(14) << setfill('.') << Scores[6][1] << setw(10) << setfill('.') << Scores[6][2] << setw(10) << setfill('.')
+        << Scores[6][3] << setw(11) << setfill('.') << Scores[6][4] << setw(8) << setfill('.') << AverageScores[6] << endl;
 
     //Bowler #8
-    cout << Names[7] << "." << setw(20) << setfill('.') << Scores[7][0] << setw(7) << setfill('.') << Scores[7][1] << setw(7) << setfill('.')
-        << Scores[7][2] << setw(8) << setfill('.') << Scores[7][3] << setw(5) << setfill('.') << AverageScores[7] << endl;
+    cout << Names[7] << "." << setw(14) << setfill('.') << Scores[7][1] << setw(10) << setfill('.') << Scores[7][2] << setw(10) << setfill('.')
+        << Scores[7][3] << setw(11) << setfill('.') << Scores[7][4] << setw(8) << setfill('.') << AverageScores[7] << endl;
 
     //Bowler #9
-    cout << Names[8] << "." << setw(20) << setfill('.') << Scores[8][0] << setw(7) << setfill('.') << Scores[8][1] << setw(7) << setfill('.')
-        << Scores[8][2] << setw(8) << setfill('.') << Scores[8][3] << setw(5) << setfill('.') << AverageScores[8] << endl;
+    cout << Names[8] << "." << setw(13) << setfill('.') << Scores[8][1] << setw(10) << setfill('.') << Scores[8][2] << setw(10) << setfill('.')
+        << Scores[8][3] << setw(11) << setfill('.') << Scores[8][4] << setw(8) << setfill('.') << AverageScores[8] << endl;
 
     //Bowler #10
-    cout << Names[9] << "." << setw(20) << setfill('.') << Scores[9][0] << setw(7) << setfill('.') << Scores[9][1] << setw(7) << setfill('.')
-        << Scores[9][2] << setw(8) << setfill('.') << Scores[9][3] << setw(5) << setfill('.') << AverageScores[9] << endl;
+    cout << Names[9] << "." << setw(10) << setfill('.') << Scores[9][1] << setw(10) << setfill('.') << Scores[9][2] << setw(10) << setfill('.')
+        << Scores[9][3] << setw(11) << setfill('.') << Scores[9][4] << setw(8) << setfill('.') << AverageScores[9] << endl;
+}
+
+void WriteToFile(string Names[10], int Scores[10][5], int AverageScores[10]) {
+    // Writing To File
+    ofstream myfile;
+    myfile.open("");
+
+    myfile << Names[0] << "." << setw(15) << setfill('.') << Scores[0][1] << setw(10) << setfill('.') << Scores[0][2] << setw(10) << setfill('.')
+        << Scores[0][3] << setw(11) << setfill('.') << Scores[0][4] << setw(8) << setfill('.') << AverageScores[0] << endl;
+    myfile << Names[1] << "." << setw(13) << setfill('.') << Scores[1][1] << setw(10) << setfill('.') << Scores[1][2] << setw(10) << setfill('.')
+        << Scores[1][3] << setw(11) << setfill('.') << Scores[1][4] << setw(8) << setfill('.') << AverageScores[1] << endl;
+    myfile << Names[2] << "." << setw(12) << setfill('.') << Scores[2][1] << setw(10) << setfill('.') << Scores[2][2] << setw(10) << setfill('.')
+        << Scores[2][3] << setw(11) << setfill('.') << Scores[2][4] << setw(8) << setfill('.') << AverageScores[2] << endl;
+    myfile << Names[3] << "." << setw(13) << setfill('.') << Scores[3][1] << setw(10) << setfill('.') << Scores[3][2] << setw(10) << setfill('.')
+        << Scores[3][3] << setw(11) << setfill('.') << Scores[3][4] << setw(8) << setfill('.') << AverageScores[3] << endl;
+    myfile << Names[4] << "." << setw(13) << setfill('.') << Scores[4][1] << setw(10) << setfill('.') << Scores[4][2] << setw(10) << setfill('.')
+        << Scores[4][3] << setw(11) << setfill('.') << Scores[4][4] << setw(8) << setfill('.') << AverageScores[4] << endl;
+    myfile << Names[5] << "." << setw(12) << setfill('.') << Scores[5][1] << setw(10) << setfill('.') << Scores[5][2] << setw(10) << setfill('.')
+        << Scores[5][3] << setw(11) << setfill('.') << Scores[5][4] << setw(8) << setfill('.') << AverageScores[5] << endl;
+    myfile << Names[6] << "." << setw(14) << setfill('.') << Scores[6][1] << setw(10) << setfill('.') << Scores[6][2] << setw(10) << setfill('.')
+        << Scores[6][3] << setw(11) << setfill('.') << Scores[6][4] << setw(8) << setfill('.') << AverageScores[6] << endl;
+    myfile << Names[7] << "." << setw(14) << setfill('.') << Scores[7][1] << setw(10) << setfill('.') << Scores[7][2] << setw(10) << setfill('.')
+        << Scores[7][3] << setw(11) << setfill('.') << Scores[7][4] << setw(8) << setfill('.') << AverageScores[7] << endl;
+    myfile << Names[8] << "." << setw(13) << setfill('.') << Scores[8][1] << setw(10) << setfill('.') << Scores[8][2] << setw(10) << setfill('.')
+        << Scores[8][3] << setw(11) << setfill('.') << Scores[8][4] << setw(8) << setfill('.') << AverageScores[8] << endl;
+    myfile << Names[9] << "." << setw(10) << setfill('.') << Scores[9][1] << setw(10) << setfill('.') << Scores[9][2] << setw(10) << setfill('.')
+        << Scores[9][3] << setw(11) << setfill('.') << Scores[9][4] << setw(8) << setfill('.') << AverageScores[9] << endl;
+
+    myfile.close();
+
 }
 
 int main()
@@ -169,9 +308,10 @@ int main()
                           {0, 0, 0, 0, 0} };
     int AverageScores[10] = { 0 };
 
-    ReadStoreData(Storing, Names, Scores);         //"In Progress"
-    //CalculatingAverage(Scores, AverageScores);   //"In Progress"
-    //OutputResults(Names, Scores, AverageScores); //"This Function Works"
+    ReadStoreData(Storing, Names, Scores);
+    CalculatingAverage(Scores, AverageScores);
+    OutputResults(Names, Scores, AverageScores);
+    WriteToFile(Names, Scores, AverageScores);
 
     return 0;
 }
